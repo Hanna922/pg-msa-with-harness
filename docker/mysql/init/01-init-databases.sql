ed703114756c1ae691ef20310cadb994efe5f128
Hanna922 <p.ping0922@gmail.com>
2026-03-21 23:24:01 +0900

    chore: remove obsolete docker configuration from project

--- docker/mysql/init/01-init-databases.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE IF NOT EXISTS merchant_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE DATABASE IF NOT EXISTS card_authorization_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE DATABASE IF NOT EXISTS bank_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
