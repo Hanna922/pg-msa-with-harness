973fbfb015842d1e5f1f004f0558365573e7a24f
Hanna922 <p.ping0922@gmail.com>
2026-03-24 12:50:03 +0900

    refactor: rename pg-service module to payment-service

--- merchant-service/src/main/java/dev/merchant/client/PgServiceClient.java
+++ merchant-service/src/main/java/dev/merchant/client/PgServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "pg-service")
+@FeignClient(name = "payment-service")
 public interface PgServiceClient {
     @PostMapping("/api/pg/approve")
     PgAuthResponse requestPaymentAuth(@RequestBody PgAuthRequest request);
