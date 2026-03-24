0f8812fdc9adfac83ad239c2ebee6e89ce89cdc4
Hanna922 <p.ping0922@gmail.com>
2026-03-24 16:45:02 +0900

    feat(payment-service): add ledger client contract

---
+++ payment-service/src/main/java/dev/pg/client/dto/CreateLedgerTransactionRequest.java
@@ -0,0 +1,30 @@
+package dev.pg.client.dto;
+
+import dev.pg.ledger.enums.ApprovalStatus;
+import dev.pg.ledger.enums.SettlementStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateLedgerTransactionRequest {
+    private String pgTransactionId;
+    private String merchantTransactionId;
+    private String merchantId;
+    private BigDecimal amount;
+    private String currency;
+    private ApprovalStatus approvalStatus;
+    private SettlementStatus settlementStatus;
+    private String acquirerType;
+    private String responseCode;
+    private String message;
+    private String approvalNumber;
+    private LocalDateTime approvedAt;
+}
