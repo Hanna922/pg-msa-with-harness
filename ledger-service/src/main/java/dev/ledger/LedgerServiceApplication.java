5afabbf16b385a4241fd9c9d8fa8f274475cbb0c
Hanna922 <p.ping0922@gmail.com>
2026-03-24 14:18:55 +0900

    feat(ledger-service): scaffold transaction ledger service

---
+++ ledger-service/src/main/java/dev/ledger/LedgerServiceApplication.java
@@ -0,0 +1,14 @@
+package dev.ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class LedgerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedgerServiceApplication.class, args);
+    }
+}
